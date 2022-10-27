1216cc648d03d3c3d634a1ca18963cc0afda82be
sujin0607 <goweg0607@gmail.com>
2022-10-27 13:38:00 +0900

    Post Request

---
+++ src/main/java/com/springboot/hello/controller/PostController.java
@@ -0,0 +1,14 @@
+package com.springboot.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    public String postExample(){
+        return "hello Post APIP";
+    }
+}
