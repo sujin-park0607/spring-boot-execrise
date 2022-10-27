b574003ab375bb91c682772e093fce56111f2042
sujin0607 <goweg0607@gmail.com>
2022-10-27 14:42:50 +0900

    Put

---
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -0,0 +1,31 @@
+package com.springboot.hello.controller;
+
+import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+//    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+//    public String postExample(){
+//        return "hello Post APIP";
+//    }
+//
+//    @PostMapping("/member")
+//    public String postMember(@RequestBody Map<String, Object> postData){
+//        StringBuilder sb = new StringBuilder();
+//        postData.entrySet().forEach(map->sb.append(map.getKey()+":"+map.getValue()+"\n"));
+//        return sb.toString();
+//    }
+
+    @PostMapping("/member3")
+    public ResponseEntity<MemberDto> putMember(@RequestBody MemberDto memberDto){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
+    }
+}
