b1c10d0a7892f216241068ca2dab899204924cc7
sujin0607 <goweg0607@gmail.com>
2022-10-28 14:44:11 +0900

    Expose password

--- src/main/java/com/springboot/hello/dao/UserDao.java
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.springboot.hello.dao;
 
 import com.springboot.hello.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -10,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
 
+@Component
 public class UserDao {
     private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate;
